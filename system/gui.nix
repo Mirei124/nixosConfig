d6a93060824e1c3a39ed36551abf27aa62652fa3
Mirei124 <3100496917@qq.com>
2025-01-17 10:57:47 +0800

    add fontconfig

--- system/gui.nix
+++ system/gui.nix
@@ -12,13 +12,58 @@ in {
     enable = true;
   };
 
-  services.displayManager = {
-    enable = true;
-    sddm = {
-      enable = true;
-      enableHidpi = true;
-      theme = "catppuccin-mocha";
-      package = lib.mkForce pkgs.kdePackages.sddm;
+  config = let
+    cfg = config.myConfig;
+  in
+    mkIf config.useGUI {
+      services.xserver = {
+        enable = true;
+      };
+
+      services.displayManager = {
+        enable = true;
+        sddm = {
+          enable = true;
+          enableHidpi = true;
+          theme = "catppuccin-mocha";
+          package = mkForce pkgs.kdePackages.sddm;
+        };
+      };
+
+      services.desktopManager.plasma6.enable = true;
+
+      programs.wayfire = {
+        enable = true;
+        plugins = with pkgs.wayfirePlugins; [
+          wcm
+          wf-shell
+        ];
+      };
+
+      i18n.inputMethod = {
+        enable = true;
+        type = "fcitx5";
+        fcitx5 = {
+          waylandFrontend = true;
+          addons = with pkgs; [
+            fcitx5-chinese-addons
+            fcitx5-pinyin-zhwiki
+            fcitx5-pinyin-moegirl
+            fcitx5-anthy # 日本語
+          ];
+        };
+      };
+
+      environment.systemPackages = with pkgs; [
+        catppuccin-sddm
+        firefox
+
+        fira-code
+      ];
+
+      home-manager.users.${cfg.username}.xresources.properties = {
+        "Xft.dpi" = 96;
+      };
     };
   };
 
