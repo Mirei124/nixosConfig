d3f1dbe7f5a4ea62abf6e4b629252ecd544546d0
Mirei124 <3100496917@qq.com>
2025-01-17 11:00:44 +0800

    refactor gui.nix

--- system/gui.nix
+++ system/gui.nix
@@ -5,11 +5,9 @@
   ...
 }
 :
-lib.mkIf config.useGUI (let
-  cfg = config.myConfig;
-in {
-  services.xserver = {
-    enable = true;
+with lib; {
+  options = {
+    useGUI = mkEnableOption "useGUI";
   };
 
   config = let
@@ -69,38 +67,4 @@ in {
         "Xft.dpi" = 96;
       };
     };
-  };
-
-  services.desktopManager.plasma6.enable = true;
-
-  programs.wayfire = {
-    enable = true;
-    plugins = with pkgs.wayfirePlugins; [
-      wcm
-      wf-shell
-    ];
-  };
-
-  i18n.inputMethod = {
-    enable = true;
-    type = "fcitx5";
-    fcitx5 = {
-      waylandFrontend = true;
-      addons = with pkgs; [
-        fcitx5-chinese-addons
-        fcitx5-pinyin-zhwiki
-        fcitx5-pinyin-moegirl
-        fcitx5-anthy # 日本語
-      ];
-    };
-  };
-
-  environment.systemPackages = with pkgs; [
-    catppuccin-sddm
-    firefox
-  ];
-
-  home-manager.users.${cfg.username}.xresources.properties = {
-    "Xft.dpi" = 96;
-  };
-})
+}
