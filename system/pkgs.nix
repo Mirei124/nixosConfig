c3b44de34d84ef567f8c4f515f8fe10dfc0532fd
Mirei124 <3100496917@qq.com>
2025-01-17 11:01:41 +0800

    update pkgs

--- system/pkgs.nix
+++ system/pkgs.nix
@@ -1,39 +1,45 @@
-{pkgs, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   programs.nix-ld.enable = true;
-  environment.systemPackages = with pkgs; [
-    zsh
-    git
-    wget
-    curl
+  environment.systemPackages = with pkgs;
+    [
+      zsh
+      git
+      wget
+      curl
 
-    alejandra
-    nix-search-cli
-    nix-tree
+      alejandra
+      nix-search-cli
+      nix-tree
 
-    neovim
-    gnumake
-    gcc
-    lua
-    fzf
-    python3
-    python312Packages.pynvim
-    nodejs_23
-    unzip
-    gnutar
-    tree-sitter
+      neovim
+      gnumake
+      gcc
+      lua
+      fzf
+      python3
+      python312Packages.pynvim
+      nodejs_23
+      unzip
+      gnutar
+      tree-sitter
 
-    # cargo
-    tree
-    htop
-    fd
-    ripgrep
-    gdu
-    lf
-    tldr
-    tmux
-
-    ## python
-    conda
-    jetbrains.pycharm-community-bin
-  ];
+      # cargo
+      tree
+      htop
+      fd
+      ripgrep
+      gdu
+      lf
+      tldr
+      tmux
+    ]
+    ++ (lib.optionals (config.networking.hostName == "wsl") [
+      ## python
+      conda
+      jetbrains.pycharm-community-bin
+    ]);
 }
