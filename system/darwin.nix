40c0b1968f0a36c3e5660a0e66b9a37117b311bc
Connor <chkycc@qq.com>
2026-02-27 16:01:08 +0800

    update

--- system/darwin.nix
+++ system/darwin.nix
@@ -12,5 +12,44 @@
     nmap
     nodejs
     uv
+    qbittorrent-enhanced
+    stats
+    obsidian
+    utm
+    miniserve
+    mpv
+    pnpm
+    nvtopPackages.apple
+    lf
+    tmux
+    yt-dlp
+    aria2
+    htop
   ];
+
+  environment.shellAliases = {
+    "nbs" = "sudo darwin-rebuild switch --flake /etc/nix-darwin";
+    "npk" = "sudo -E nvim /etc/nix-darwin/system/darwin.nix";
+  };
+
+  services.openssh = {
+    enable = true;
+    extraConfig = ''
+      PermitRootLogin no
+      PasswordAuthentication no
+      KbdInteractiveAuthentication no
+    '';
+  };
+
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 5d";
+  };
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 }
